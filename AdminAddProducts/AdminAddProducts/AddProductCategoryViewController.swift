d14d0ce5ee7b1b15f569cc2372246ad703496dcc
Ashish <ashish@norteastern.edu>
2021-04-14 08:31:22 +0530

    Adding new File for product Categories

---
+++ AdminAddProducts/AdminAddProducts/AddProductCategoryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddProductCategoryViewController.swift
+//  AdminAddProducts
+//
+//  Created by Ashish Ashish on 14/04/21.
+//
+
+import UIKit
+
+class AddProductCategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+   
+}
