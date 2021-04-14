fe5df054a6730475597ccf14cf8724243a6ae77b
Ashish <ashish@norteastern.edu>
2021-04-14 07:38:17 +0530

    Checking in code for Collection View for horizontal scrolling

---
+++ SampleCollectionView/SampleCollectionView/SampleCollectionView/ImageCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCollectionViewCell.swift
+//  SampleCollectionView
+//
+//  Created by Ashish Ashish on 14/04/21.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var imgCell: UIImageView!
+    
+}
--- SampleCollectionView/SampleCollectionView/SampleCollectionView/ViewController.swift
+++ SampleCollectionView/SampleCollectionView/SampleCollectionView/ViewController.swift
@@ -7,12 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    let arr = [ UIImage(named: "Beverages"),
+                UIImage(named: "Breakfast"),
+                UIImage(named: "Chocolates"),
+                UIImage(named: "Fruits"),
+                UIImage(named: "Household"),
+                UIImage(named: "Jams"),
+                UIImage(named: "Readymeals"),
+                UIImage(named: "Snacks"),
+                UIImage(named: "Vegetables")
+    
+    ]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        arr.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as! ImageCollectionViewCell
+        
+        cell.imgCell.image = arr[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    
 
 
 }
