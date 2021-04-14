9f8c4a9b7bbafa53aa071b072f6ea5d3471dd033
Ashish <ashish@norteastern.edu>
2021-04-14 08:39:26 +0530

    Checking in code for adding code for uploading image to storage

--- AdminAddProducts/AdminAddProducts/AddProductCategoryViewController.swift
+++ AdminAddProducts/AdminAddProducts/AddProductCategoryViewController.swift
@@ -6,13 +6,73 @@
 //
 
 import UIKit
+import Firebase
 
-class AddProductCategoryViewController: UIViewController {
+class AddProductCategoryViewController: UIViewController,  UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
 
+    @IBOutlet weak var imgView: UIImageView!
+    
+    @IBOutlet weak var txtCategory: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    @IBAction func selectImageAction(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.allowsEditing = false
+            
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+    {
+        if let pickedImage = info[.originalImage] as? UIImage{
+            imgView.image = pickedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func uploadImageAction(_ sender: Any) {
+        
+        let name = txtCategory.text
+        
+        if name == "" {
+            return
+        }
+        
+        let storageRef = Storage.storage().reference()
+        // Data in memory
+        let data = imgView.image?.pngData()
+
+        // Create a reference to the file you want to upload
+        let categoryRef = storageRef.child("categories/" + name! + ".png")
+
+        // Upload the file to the path "images/rivers.jpg"
+        let uploadTask = categoryRef.putData(data!, metadata: nil) { (metadata, error) in
    
+            if error != nil {
+                print(error?.localizedDescription)
+                return
+            }
+   
+          // You can also access to download URL after upload.
+            categoryRef.downloadURL { (url, error) in
+            guard let downloadURL = url else {
+              // Uh-oh, an error occurred!
+              return
+            }
+                
+            print(downloadURL)
+          }
+        }
+        
+    }
+    
+    
 }
